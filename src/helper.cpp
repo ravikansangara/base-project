f237408a8bf4cf2420e1eaf327377a90c0e87fb0
ravikansangara <kansangara.ravi@gmail.com>
2024-10-22 11:34:40 +0530

    Add project files

---
+++ src/helper.cpp
@@ -0,0 +1,8 @@
+#include "helper.h"
+
+#include <iostream>
+
+void helper_function_one()
+{
+    std::cout << __func__ << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "helper.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    helper_function_one();
+
+    return 0;
+}
