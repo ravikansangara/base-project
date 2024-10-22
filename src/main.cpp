ff995e665189bb612922ac35a85035746140b7a1
ravikansangara <kansangara.ravi@gmail.com>
2024-10-22 15:32:10 +0530

    Use submodules in the base project

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
 #include "helper.h"
+#include "libone.h"
+#include "libtwo.h"
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
 
     helper_function_one();
+
+    libone_function();
+
+    libtwo_function();
 
     return 0;
 }
