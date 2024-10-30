5e7a98db4b3ccc6894eb059a4f6ca20deb134788
ravikansangara <kansangara.ravi@gmail.com>
2024-10-30 15:57:11 +0530

    Add some in error code to see codeQL results

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
+#include <cstring>
 #include <iostream>
 
 #include "helper.h"
 #include "libone.h"
 #include "libtwo.h"
+
+void example_buffer_overflow()
+{
+    char buffer[10];
+
+    // CodeQL will flag this as writing beyond the buffer's capacity
+    strcpy(buffer, "This is too long for buffer");
+}
+
+void example_uninitialized_variable()
+{
+    int x;                        // Variable 'x' is not initialized
+    std::cout << x << std::endl;  // CodeQL will flag the use of an uninitialized variable
+}
+
+void example_memory_leak()
+{
+    int* ptr = new int[10];
+    // No call to delete[] ptr, which CodeQL will detect as a memory leak
+}
+
+void example_sql_injection(const std::string& userInput)
+{
+    std::string query = "SELECT * FROM users WHERE username = '" + userInput + "';";
+
+    // CodeQL will flag this as a potential SQL injection risk
+    std::cout << "Executing query: " << query << std::endl;
+}
 
 int main()
 {
@@ -13,6 +42,12 @@ int main()
     libone_function();
 
     libtwo_function();
+
+    // FUnctions to test CodeQL quality
+    example_buffer_overflow();
+    example_uninitialized_variable();
+    example_memory_leak();
+    example_sql_injection("Alice");
 
     return 0;
 }
